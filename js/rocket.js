bdb4e293c00611b020104ae2f0d062683673b5f5
Jona van der Pal <57063625+jojojona372@users.noreply.github.com>
2020-02-20 22:20:40 +0100

    V0.1.01
    Added a rocket rotation indicator in the upper left corner.

--- js/rocket.js
+++ js/rocket.js
@@ -10,40 +10,40 @@ class Rocket {
 		this.w = w;
 		this.h = h;
 		this.color = color;
-		
+
 		//This is the angle from the horizontal plane
 		this.angle = 90;
-		
+
 		this.mass = new Mass(eMass, pMass, fMass, fMax);
 		// this.mass.empty = eMass;
 		// this.mass.payload = pMass;
 		// this.mass.fuel = fMass;
 		// this.mass.fuelMax = fMax;
 		// this.mass.total = eMass+pMass+fMass;
-		
+
 		this.pos = new Vector(0, 0, 0, 0);
 		// this.pos.x = x;
 		// this.pos.y = y;
 		// this.pos.xDelta = 0;
 		// this.pos.yDelta = 0;
-		
+
 		this.vel = new Vector(0, 0, 0, 0);
 		// this.vel.x = 0;
 		// this.vel.y = 0;
 		// this.vel.xDelta = 0;
 		// this.vel.yDelta = 0;
-		
+
 		this.acc = new Vector(0, 0);
 		// this.acc.x = 0;
 		// this.acc.y = 0;
-		
+
 		this.thrust = new Thruster(thrust);
 		// this.thrust.total = 0;
 		// this.thrust.x = 0;
 		// this.thrust.y = 0;
-		
+
 		this.area = new Vector(0, 0);
-		
+
 		//I have not yet been able to find a method to calculate this accurately, so I will use this value for the time being.
 		this.cDrag = 0.80;
 		this.drag = new Vector(0, 0);
@@ -51,15 +51,15 @@ class Rocket {
 		this.force = new Vector(0, 0);
 		// this.force.x = 0;
 		// this.force.y = 0;
-		
+
 	}
 
 	updateHeight(){
 		//this.y = height - (0.000000000000001 * frameCount ** 9.81) - this.h;
 	}
-	
+
 	draw() {
-		
+
 		if(keyIsDown(keyW)) {
 			// print('W');
 			// this.vel.y += 10;
@@ -67,7 +67,7 @@ class Rocket {
 		} else {
 			this.thrust.enabled = false;
 		}
-		
+
 		if(keyIsDown(keyA)) {
 			// print('A');
 			//this.pos.x -= 10;
@@ -82,10 +82,10 @@ class Rocket {
 			//this.pos.x += 10;
 			this.angle -= 5;
 		}
-		
+
 		//this.vel.x = this.vel.x | 0;
 		//this.vel.y = this.vel.y | 0;
-		
+
 		// if(keyIsDown(keyW)) {
 			// //print('W');
 			// this.vel.y += 10;
@@ -102,19 +102,19 @@ class Rocket {
 			// //print('D');
 			// this.vel.x += 10;
 		// }
-		
+
 		// calcPosition(this, environment);
 		doCalculations(rocket);
-		
+
 		//this.updateHeight();
-		
+
 		//if (this.y < -this.h) {
 		//	frameCount = 0;
 		//}
 		//print(rocket.acc.x);
-		
-		fill(this.color);
+
 		push();
+		fill(this.color);
 		translate(this.pos.x + 0.5 * this.w, -this.pos.y + 600 + 0.5 * this.h);
 		rotate(-this.angle + 90);
 		translate(-0.5 * this.w, -0.5 * this.h);
--- js/sketch.js
+++ js/sketch.js
@@ -12,10 +12,10 @@ function setup() {
 	frameRate(60);
 	timeStep = 1/15;
 	angleMode(DEGREES);
-	
+
 	//constructor(name = "New simulation", gravity, airDensity)
 	environment = new Environment("Earth", -9.81, 1.225);
-	
+
 	//constructor(name, w, h, thrust, eMass, pMass, fMass, fMax, color = 255)
 	rocket = new Rocket("Rocket", 50, 100, 50, 1, 0, 0, 1);
 }
@@ -27,12 +27,21 @@ function draw() {
 	text("X: " + rocket.pos.x.toFixed(1), 20, 20);
 	text("Y: " + rocket.pos.y.toFixed(1), 20, 40);
 	text("Angle: " + rocket.angle.toFixed(1) + "°", 20, 60);
+	
+	push();
+	fill(rocket.color);
+	translate(50 + 0.25 * rocket.w, 80 + 0.25 * rocket.h);
+	rotate(-rocket.angle + 90);
+	translate(-0.25 * rocket.w, -0.25 * rocket.h);
+	rect(0, 0, 0.5 * rocket.w, 0.5 * rocket.h);
+	pop();
+
 	rocket.draw();
 }
 
 
 function mouseDragged(){
-	 
+
 }
 
 /*
